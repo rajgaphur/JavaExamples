906debb3ad004b7aa4c81c041d6bd0a26ec193d7
Rajgaphur <rajgaphur143@gmail.com>
2025-01-10 13:14:25 +0530

    added ExGit1.java from eclipse under com.examples pkg

---
+++ src/test/java/com/examples/ExGit1.java
@@ -0,0 +1,12 @@
+package com.examples;
+
+public class ExGit1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("First file for using repository");
+
+	}
+
+}
